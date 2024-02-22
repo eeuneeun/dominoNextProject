476f25983cc4be6961c53dac7142b2fa444531c3
woneunjae <chnof.ejwon@gmail>
2024-02-22 17:02:23 +0900

    NextJS14 + TanstackQuery5 적용 세팅

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import styles from '@/app/ui/home.module.css';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      
+    </main>
+  );
+}
---
+++ app/types/User.tsx
@@ -0,0 +1,5 @@
+export type User = {
+  id: number;
+  nme: string;
+  emil: string;
+};
