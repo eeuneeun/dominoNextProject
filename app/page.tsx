2e615cf16125f6fc43f1fb5b44a7b8c5bdf156fe
woneunjae <chnof.ejwon@gmail>
2024-02-29 16:34:19 +0900

    폴더 구조 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import AcmeLogo from '@/app/ui/acme-logo';
 import styles from '@/app/ui/home.module.css';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
